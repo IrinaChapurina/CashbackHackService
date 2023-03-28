b2154006a8daab08b6aa65f232e9729d4106762d
Irina Chapurina <chapurina-85@yandex.ru>
2023-03-28 21:18:42 +0300

    add tests main

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,24 +1,26 @@
+import org.junit.jupiter.api.Test;
 import ru.netology.service.CashbackHackService;
 
 public class CashbackHackServiceTest {
 
+@Test
 
-
-    public void testRemain (){
+    public void testRemain0() {
         CashbackHackService service = new CashbackHackService();
-        int amout = 1000;
-        int actual = service.remain(amout);
-        int expected = 0;
-        assertEguals(actual, expected);
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
     }
 
-    public void testRemainWithCashback(){
+    @Test
+
+    public void testRemain10() {
         CashbackHackService service = new CashbackHackService();
-        int amout = 900;
-        int actual = service.remain(amout);
-        int expected = 100;
-        assertEguals(actual, expected); //почему красным?
-
-
+        int amount = 10;
+        int actual = service.remain(amount);
+        int expected = 990;
+        assertEquals(actual, expected);
     }
+
 }
